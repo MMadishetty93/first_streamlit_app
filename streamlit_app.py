9ef37d5fdf2c18ed96caa00fbca18aaa9a4c675e
MMadishetty93 <149480428+MMadishetty93@users.noreply.github.com>
2023-11-02 13:53:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ myFruitList = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.multiselect("Please select a fruit:",list(myFruitList.Fruit))
 streamlit.dataframe(myFruitList)
 
-streamlit.header('FruityVice fruit advice')
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input(What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
-fruit_choice = streamlit.text_input('Please select a fruit of your choice','kiwi')
-streamlit.write(fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
