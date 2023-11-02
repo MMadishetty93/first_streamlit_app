ddd55132f51f06eeb2d722cc127e5ddb9d380911
MMadishetty93 <149480428+MMadishetty93@users.noreply.github.com>
2023-11-02 13:47:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.multiselect("Please select a fruit:",list(myFruitList.Fruit))
 streamlit.dataframe(myFruitList)
 
 streamlit.header('FruityVice fruit advice')
-fruityvice_respose = requests.get("https://fruityvice.com/api/fruit/")
+fruityvice_respose = requests.get("https://fruityvice.com/api/frui/")
 
 streamlit.text(fruityvice_respose.json())
 
