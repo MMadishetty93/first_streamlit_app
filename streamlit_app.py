78288bddab3bced684299845f02a2162ac4339f1
MMadishetty93 <149480428+MMadishetty93@users.noreply.github.com>
2023-11-02 13:29:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ myFruitList = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.multiselect("Please select a fruit:",list(myFruitList.Fruit))
 streamlit.dataframe(myFruitList)
 
+streamlit.header('FruityVice fruit advice')
 fruityvice_respose = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.text(fruityvice_respose.json())
